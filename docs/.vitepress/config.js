46caf1fd13e4c25aede6fc444dbb2dd4d8fc88e7
Lpuena <g2534160060@163.com>
2023-03-02 12:53:59 +0800

    侧边栏修改

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -41,12 +41,12 @@ export default {
             // This shows `/guide/index.md` page.(必须有index.md)
             {text: 'Vue3简介 ', link: '/vue/'},
             {text: 'Vue3带来了什么', link: '/vue/one.md'},
-            {text: '创建', link: '/vue/two.md'},
-            {text: '常用 Composition API', link: '/vue/three.md'},
-            {text: '其它 Composition API', link: '/vue/four.md'},
-            {text: 'Composition API 的优势 ', link: '/vue/five.md'},
-            {text: '新的组件 ', link: '/vue/six.md'},
-            {text: '其它', link: '/vue/seven.md'},
+            {text: '一、创建', link: '/vue/two.md'},
+            {text: '二、常用 Composition API', link: '/vue/three.md'},
+            {text: '三、其它 Composition API', link: '/vue/four.md'},
+            {text: '四、Composition API 的优势 ', link: '/vue/five.md'},
+            {text: '五、新的组件 ', link: '/vue/six.md'},
+            {text: '六、其它', link: '/vue/seven.md'},
           ]
         }
       ]
