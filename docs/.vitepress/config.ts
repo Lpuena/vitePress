a2d4802f56d985ad52205d7ab83c766664746a09
Lpuena <lpuena@outlook.com>
2024-09-13 14:59:39 +0800

    chore: cancel zh-Hans

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import getSidebar from "./config/sidebar";
 
 
 export default defineConfig({
-  lang: 'zh-Hans',
+  // lang: 'zh-Hans',
   title: 'FrontEnd Blog',
   description: 'A front-end blog powered by VitePress',
   head: [
@@ -19,23 +19,23 @@ export default defineConfig({
   themeConfig: {
     search: {
       provider: "local",
-      options: {
-        translations: {
-          button: {
-            buttonText: '搜索文档',
-            buttonAriaLabel: '搜索文档'
-          },
-          modal: {
-            noResultsText: '无法找到相关结果',
-            resetButtonTitle: '清除查询条件',
-            footer: {
-              selectText: '选择',
-              navigateText: '切换',
-              closeText: '关闭'
-            }
-          }
-        }
-      }
+      // options: {
+      //   translations: {
+      //     button: {
+      //       buttonText: '搜索文档',
+      //       buttonAriaLabel: '搜索文档'
+      //     },
+      //     modal: {
+      //       noResultsText: '无法找到相关结果',
+      //       resetButtonTitle: '清除查询条件',
+      //       footer: {
+      //         selectText: '选择',
+      //         navigateText: '切换',
+      //         closeText: '关闭'
+      //       }
+      //     }
+      //   }
+      // }
     },
     logo: '/Lp-1.svg',
 
@@ -45,19 +45,19 @@ export default defineConfig({
     nav: getNavs(),
     sidebar:getSidebar(),
     lastUpdated: {
-      text: '最后更新于',
+      // text: '最后更新于',
       formatOptions: {
         dateStyle: 'short',
         timeStyle: 'medium'
       }
     },
     docFooter: {
-      prev: '上一页',
-      next: '下一页'
+      // prev: '上一页',
+      // next: '下一页'
     },
 
     outline: {
-      label: '页面导航',
+      // label: '页面导航',
       level: [2, 4],
     },
   },
