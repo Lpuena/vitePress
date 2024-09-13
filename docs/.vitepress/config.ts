7e658a35a89024eaf4b23631e8d0f2ab5ba165bd
Lpuena <lpuena@outlook.com>
2024-09-13 14:30:39 +0800

    chore: change config file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,65 @@
-import {defineConfig} from 'vitepress'
+import { defineConfig } from 'vitepress'
 import getNavs from "./config/nav";
 import sidebar from "./config/sidebar";
 
 
 export default defineConfig({
-  // title:'Vite',
-  head: [
-    // ['link', {rel: 'icon', type: 'image/svg+xml', href: 'https://lpuena.github.io/vitePress/32.png'}],
-    ['link', {rel: 'icon', type: 'image/svg+xml', href: 'https://lpuena.github.io/vitePress/Lp-1.svg'}],
-  ],
+  lang: 'zh-Hans',
+  title: 'FrontEnd Blog',
+  description: 'A front-end blog powered by VitePress',
+  // head: [
+  //   // ['link', {rel: 'icon', type: 'image/svg+xml', href: 'https://lpuena.github.io/vitePress/32.png'}],
+  //   ['link', {rel: 'icon', type: 'image/svg+xml', href: 'https://lpuena.github.io/vitepress/Lp-1.svg'}],
+  // ],
   lastUpdated: true,
   markdown: {
     lineNumbers: true,
   },
-  base: '/vitePress/',
+  base: '/vitepress/',
   themeConfig: {
     search: {
       provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+      }
     },
-    outline: [2, 4],//侧边栏深度:数字或者deep
+    logo: '/Lp-1.svg',
+
     // outlineTitle: '文章目录',
     siteTitle: 'FrontEnd',
     // logo: '/32.png',
     nav: getNavs(),
     sidebar,
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      }
+    },
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+
+    outline: {
+      label: '页面导航',
+      level: [2, 4],
+    },
   },
 
 })
--- docs/git/npm/index.md
+++ docs/git/npm/index.md
@@ -1,34 +1,12 @@
 # 好用的NPM包
-## [ni](https://github.com/antfu/ni)
+- [ni](https://github.com/antfu/ni)
 自动判断是采用 `npm` 还是 `yarn` 还是 `pnpm`
-```shell
-npm i -g @antfu/ni
-```
-示例：(安装)
-```shell
-ni
 
-# npm install
-# yarn install
-# pnpm install
-# bun install
-```
-运行：
-```shell
-nr dev --port=3000
-
-# npm run dev -- --port=3000
-# yarn run dev --port=3000
-# pnpm run dev --port=3000
-# bun run dev --port=3000
-```
-
-## ts-node
+- [ts-node](https://www.npmjs.com/package/ts-node)
 直接运行ts文件
-```shell
-npm install -g ts-node
-```
-示例：
-```shell
-ts-node server.ts
-```
+
+- [tsx](https://www.npmjs.com/package/tsx)
+TypeScript Execute (tsx): The easiest way to run TypeScript in Node.js
+
+- [nrm](https://www.npmjs.com/package/nrm)
+NPM 注册表管理器
